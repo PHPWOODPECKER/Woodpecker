c4adccdb24663a620c7df7324cc97c5712545cec
Woodpecker <derkob.89@gmail.com>
2025-05-14 14:40:16 +0330

    Update Routes.php

--- Project/Web/Routes.php
+++ Project/Web/Routes.php
@@ -1,29 +1,7 @@
 <?php
-use Woodpecker\Controllers\User;
-use Woodpecker\DataBase\Table;
+use Woodpecker\Controllers\Test;
 
-// view('native');
-// $router->url('/home/', function(){
-//   view('home');
-// });
-// $router->url('/about/', function(){
-//   view('about');
-// });
-// $router->url('/contact/', function(){
-//   view('contact');
-// });
-// $router->url('/portfolio/', function(){
-//   view('portfolio');
-// });
-
-
-$router->url(['url' => '/test', 'rate' => 10], function() use ($router){
-  $router->get(['name', 'age'], function($name, $age){
-    Table::init();
-    Table::save('users', [
-      'name' => $name,
-      'age' => $age
-      ]);
-  });
-});
-
+$router->url('/test', [
+     "class" => Controllers\Test::class, 
+     "method" => "hello"
+ ]);
